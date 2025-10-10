3721df08981c49bef0ca680834a71648c59792b2
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2025-10-10 12:29:23 +0100

    Cleanup

--- lib/logging.sh
+++ lib/logging.sh
@@ -2,10 +2,10 @@ NO_COLOR="\033[0m"
 INFO_COLOR="\033[1;32m"
 ERR_COLOR="\033[0;31m"
 
-function log_info() {
+function log_info {
   echo -e "${INFO_COLOR}$1${NO_COLOR}"
 }
 
-function log_error() {
+function log_error {
   echo -e "${ERR_COLOR}$1${NO_COLOR}"
 }
--- lib/utils.sh
+++ lib/utils.sh
@@ -1,3 +1,3 @@
-get_latest_release() {
+function get_latest_release {
   curl -fsSL https://api.github.com/repos/"$1"/releases/latest | grep '"tag_name":' | cut -d'"' -f4
 }
