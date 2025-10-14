9f2e0581dd2f044aa35d05a2688fcc0a97f03df5
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2025-10-14 21:28:46 +0100

    Fix dotfile symlinks and improve git cloning

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,3 +1,17 @@
+# Gets the latest tag for a given github repo
 function get_latest_release {
   curl -fsSL https://api.github.com/repos/"$1"/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+
+# Clones if not already cloned or pulls updates if exists
+function get_latest_git {
+  local repo target
+  repo=$1
+  target=$2
+
+  if [[ -d "$target/.git" ]]; then
+    git -C $target pull
+  else
+    git clone $repo $target
+  fi
 }
