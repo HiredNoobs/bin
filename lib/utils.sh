ff46a4837fb20776c0258f66c19225ecbcd6bb0b
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2025-10-14 22:06:59 +0100

    Make docker group setup idempotent

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,3 +1,6 @@
+# General use utility functions, does nothing by itself.
+# There shouldn't be any distro specific stuff in here...
+
 # Gets the latest tag for a given github repo
 function get_latest_release {
   curl -fsSL https://api.github.com/repos/"$1"/releases/latest | grep '"tag_name":' | cut -d'"' -f4
@@ -13,5 +16,27 @@ function get_latest_git {
     git -C $target pull
   else
     git clone $repo $target
+  fi
+}
+
+# Creates a group if one doesn't already exist
+function create_group {
+  local group
+  group=$1
+
+  if ! getent group "$group" >/dev/null; then
+    log_info "Creating group '$group'."
+    sudo groupadd "$group"
+  fi
+}
+
+# Adds a given user to a group if not already in the group
+function add_user_to_group {
+  local user group
+  user=$1
+  group=$2
+
+  if ! id -nG "$user" | grep -qw "$group"; then
+    sudo usermod -aG "$group" "$user"
   fi
 }
