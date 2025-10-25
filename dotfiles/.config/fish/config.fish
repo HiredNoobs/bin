f33702d2ea8b606e31b7788e29f5e3d71b13265a
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2025-10-25 16:48:57 +0100

    Add starship and fish for hyprland and fix tmux issue with shell switching

---
+++ dotfiles/.config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Remove greeting
+    set fish_greeting
+
+    # Use starship
+    starship init fish | source
+
+    # Update PATH
+    fish_add_path ~/bin/bin
+
+    # Env vars (-x = export)
+    set -x SHELL $(which fish)  # For tmux
+
+    # Aliases
+    alias "tm" "tmux new-session \; split-window -h -p 50 \; select-pane -L \; attach"
+end
